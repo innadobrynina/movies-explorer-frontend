0cfc1d896d6be295c86c29a762a58239822e9856
Inna Dobrynina <a-ya-dobraya@yandex.ru>
2021-09-29 01:26:09 +0300

    aboutProject

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,29 @@
+import './AboutProject.css';
+
+function AboutProject() {
+    return (
+      <section className="about-project">
+          <div className="about-project__container">
+              <h2 className="about-project__title">О проекте</h2>
+              <div className="about-project__info">
+                <div className="about-project__column">
+                    <h3 className="about-project__column-title">Дипломный проект включал 5 этапов</h3>
+                    <p className="about-project__column-text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                </div>
+                <div className="about-project__column">
+                    <h3 className="about-project__column-title">На выполнение диплома ушло 5 недель</h3>
+                    <p className="about-project__column-text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                </div>
+              </div>
+              <div className="about-project__time">
+                <div className="about-project__part about-project__part_back">1 неделя</div>
+                <div className="about-project__part about-project__part_front">4 недели</div>
+                <div className="about-project__caption about-project__caption_back">Back-end</div>
+                <div className="about-project__caption about-project__caption_front">Front-end</div>
+              </div>     
+          </div>
+      </section>
+    );
+  }
+  
+  export default AboutProject;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
 
 function App() {
   return (
     <>
       <Header />
       <Promo />
+      <AboutProject />
     </>
   );
 }
